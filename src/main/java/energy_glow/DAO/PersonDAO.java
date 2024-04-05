06a207e37b465930fe3c7ebe800eadaa240159f5
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-04-05 19:15:47 +0500

    MVC first steps

---
+++ src/main/java/energy_glow/DAO/PersonDAO.java
@@ -0,0 +1,31 @@
+package energy_glow.DAO;
+
+import energy_glow.Models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>(Arrays.asList(
+                new Person(++PEOPLE_COUNT, "Tom"),
+                new Person(++PEOPLE_COUNT, "Mike"),
+                new Person(++PEOPLE_COUNT, "Bob"),
+                new Person(++PEOPLE_COUNT, "Katy")
+                ));
+    }
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+}
---
+++ src/main/java/energy_glow/Models/Person.java
@@ -0,0 +1,16 @@
+package energy_glow.Models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
