533a7b9f0142dfdfd5a22453d7d0c17bc6021f8a
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-04-16 22:02:42 +0500

    jdbc create read completed

--- src/main/java/energy_glow/DAO/PersonDAO.java
+++ src/main/java/energy_glow/DAO/PersonDAO.java
@@ -3,43 +3,86 @@ package energy_glow.DAO;
 import energy_glow.Models.Person;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
-    private List<Person> people;
 
-    {
-        people = new ArrayList<>(Arrays.asList(
-                new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"),
-                new Person(++PEOPLE_COUNT, "Mike", 52, "mike@mail.ru"),
-                new Person(++PEOPLE_COUNT, "Bob", 18, "bob@mail.ru"),
-                new Person(++PEOPLE_COUNT, "Katy", 34, "katy@mail.ru")
-                ));
+    private static final String URL = "jdbc:postgresql://localhost:5432/jdbc_learning";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "mercy07";
+
+    private static Connection connection;
+
+    static{
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
     public List<Person> index(){
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while(resultSet.next()){
+                Person person = new Person();
+
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+                person.setAge(resultSet.getInt("age"));
+
+                people.add(person);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return people;
     }
 
     public Person show(int id){
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        //return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return null;
     }
 
     public void save(Person person){
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = String.format("INSERT INTO Person (name, age, email) VALUES('%s', %d, '%s');", person.getName(),
+                    person.getAge(), person.getEmail());
+            statement.executeUpdate(SQL);
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public void update(int id, Person updatedPerson){
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatedPerson.getName());
-        personToBeUpdated.setAge(updatedPerson.getAge());
-        personToBeUpdated.setEmail(updatedPerson.getEmail());
+//        Person personToBeUpdated = show(id);
+//        personToBeUpdated.setName(updatedPerson.getName());
+//        personToBeUpdated.setAge(updatedPerson.getAge());
+//        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
     public void delete(int id){
-        people.removeIf(p -> p.getId() == id);
+        //people.removeIf(p -> p.getId() == id);
     }
 }
---
+++ src/main/sql/person.sql
@@ -0,0 +1,14 @@
+create table Person(
+                       id serial8 primary key,
+                       name varchar(30),
+                       age int4,
+                       email varchar(30)
+);
+
+insert into Person (name, age, email) VALUES ('Tom', 23, 'tom@mail.ru'),
+                                             ('Bob', 55, 'bob@mail.ru'),
+                                             ('John', 36, 'john@mail.ru');
+
+insert into Person (name, age, email) VALUES ('Peter', 15, 'peter@gmail.com');
+
+select * from Person;
