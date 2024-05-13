941fab14a89f2d1dc5eff21a3163a10927838d9e
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-05-13 23:03:31 +0500

    cast simple class into Entity

--- src/main/java/energy_glow/Models/Person.java
+++ src/main/java/energy_glow/Models/Person.java
@@ -1,36 +1,45 @@
 package energy_glow.Models;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "person")
 public class Person {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @NotEmpty(message = "Name can't be empty")
     @Size(min = 2, max = 30, message = "Length of name should be greater than 2 and lower than 30")
+    @Column(name = "name")
     private String name;
 
     @Min(value = 0, message = "Age can't be negative")
+    @Column(name = "age")
     private int age;
 
     @NotEmpty(message = "Email can't be empty")
     @Email
+    @Column(name = "email")
     private String email;
 
     // Country, City, Postal code (6 digits)
     @NotEmpty(message = "Address can't be empty")
     @Pattern(regexp = "[A-Z]\\w+,\\s[A-Z]\\w+,\\s\\d{6}", message = "Your address should be in this format: " +
             "Country, City, Postal Code (6 digits)")
+    @Column(name = "address")
     private String address;
 
-    public Person(){
+    public Person() {
 
     }
-    public Person(int id, String name, int age, String email, String address) {
-        this.id = id;
+
+    public Person(String name, int age, String email, String address) {
         this.name = name;
         this.age = age;
         this.email = email;
