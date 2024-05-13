394325ac3b97e8e3ce999048853dcbceb9427242
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-05-13 23:04:13 +0500

    absent of Optional<Person> show method

--- src/main/java/energy_glow/util/PersonValidator.java
+++ src/main/java/energy_glow/util/PersonValidator.java
@@ -25,8 +25,8 @@ public class PersonValidator implements Validator {
     public void validate(Object target, Errors errors) {
         Person person = (Person) target;
 
-        if(personDAO.show(person.getEmail()).isPresent()){
+        /*if(personDAO.show(person.getEmail()).isPresent()){
             errors.rejectValue("email", "500", "This email already exists");
-        }
+        }*/
     }
 }
